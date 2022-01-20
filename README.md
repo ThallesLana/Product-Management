64f6219e898a710b00bdc28ece03a51aec65681b
ThallesLana <thalles.lana1@gmail.com>
2022-01-19 21:24:18 -0300

    alpha 1.2.1

--- README.md
+++ README.md
@@ -1,24 +1,2 @@
-![Product Management](https://user-images.githubusercontent.com/57325727/150052006-2c79b433-79cc-41e4-979e-a3f2df1ea3ad.png)
-
-# 👨‍💼 Product Management
-
-### This is a project made with angular on the frontend and json-server on the backend with the intention of deepening my knowledge of the framework angular.
-
-# ✅ Project Status:
-### 🚧 Product Management 👨‍💼 Under construction... 🚧
-
-# 💻 Languages and Tools used in the project:
-![Angular Badge](https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white)
-![CSS3 Badge](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
-![GIT Badge](https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white)
-![HTML5 Badge](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
-![JSON Badge](https://img.shields.io/badge/json-5E5C5C?style=for-the-badge&logo=json&logoColor=white)
-![Typescript Badge](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)
-![VS Code Badge](https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white)
-
-# 👨‍💻 Author:
-
-<table>
-  <tr>
-    <td align="center"><a href="https://github.com/ThallesLana"><img src="https://avatars.githubusercontent.com/u/57325727?v=4" width="100px;" alt=""/><br /><sub><b>Thalles Fernandes</b></sub></a><br /><a href="https://github.com/ThallesLana" title="Thalles">💻🚀</a></td>
-</table>
+# Product-Management
+This is a project made with angular on the frontend and json-server on the backend with the intention of deepening my knowledge of the framework angular.
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,42 @@
+// core
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+// platform-browser
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// material
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// http
+import { HttpClientModule } from '@angular/common/http'; 
+
+// app
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
+// components 
 import { FooterComponent } from './components/template/footer/footer.component';
+import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
+// directive
+import { GreenDirective } from './directives/green.directive';
+
+// views
 import { HomeComponent } from './views/home/home.component';
 import { ProductsComponent } from './views/products/products.component';
-import { GreenDirective } from './directives/green.directive';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+// forms
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -40,7 +58,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatSidenavModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
